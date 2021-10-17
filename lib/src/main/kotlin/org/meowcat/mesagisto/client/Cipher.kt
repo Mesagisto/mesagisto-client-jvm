ac38bdf8e4b43cb64b67d7f5c98407ef5354c568
itsusinn <itsusinn@foxmail.com>
2021-10-17 13:52:56 +0800

    feat(security): support aes-256-gcm cipher

---
+++ lib/src/main/kotlin/org/meowcat/mesagisto/client/Cipher.kt
@@ -0,0 +1,44 @@
+package org.meowcat.mesagisto.client
+
+import org.bouncycastle.crypto.engines.AESEngine
+import org.bouncycastle.crypto.modes.GCMBlockCipher
+import org.bouncycastle.crypto.params.AEADParameters
+import org.bouncycastle.crypto.params.KeyParameter
+import java.security.MessageDigest
+import java.security.SecureRandom
+
+object Cipher {
+  private lateinit var KEY: KeyParameter
+  internal lateinit var RAW_KEY: String
+  private val SECURE_RANDOM: SecureRandom by lazy { SecureRandom() }
+  fun newNonce(): ByteArray {
+    val nonce = ByteArray(12)
+    SECURE_RANDOM.nextBytes(nonce)
+    return nonce
+  }
+  fun init(key: String) {
+    RAW_KEY = key
+    val digest = MessageDigest.getInstance("SHA-256")
+    val key256 = digest.digest(key.toByteArray())
+    KEY = KeyParameter(key256)
+  }
+  fun encrypt(plaintext: ByteArray, nonce: ByteArray): ByteArray {
+    val cipher = GCMBlockCipher(AESEngine())
+    val parameters = AEADParameters(KEY, 128, nonce)
+    cipher.init(true, parameters)
+    val encryptedBytes = ByteArray(cipher.getOutputSize(plaintext.size))
+    val retLen = cipher.processBytes(plaintext, 0, plaintext.size, encryptedBytes, 0)
+    cipher.doFinal(encryptedBytes, retLen)
+    return encryptedBytes
+  }
+
+  fun decrypt(ciphertext: ByteArray, nonce: ByteArray): ByteArray {
+    val cipher = GCMBlockCipher(AESEngine())
+    val parameters = AEADParameters(KEY, 128, nonce)
+    cipher.init(false, parameters)
+    val plainBytes = ByteArray(cipher.getOutputSize(ciphertext.size))
+    val retLen = cipher.processBytes(ciphertext, 0, ciphertext.size, plainBytes, 0)
+    cipher.doFinal(plainBytes, retLen)
+    return plainBytes
+  }
+}
