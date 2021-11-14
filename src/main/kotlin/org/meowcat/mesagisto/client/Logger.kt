641854793d5825eb0e49f52b5d345e0cf06173d7
itsusinn <itsusinn@foxmail.com>
2021-11-14 15:42:30 +0800

    fix(logger): wrong level compare

--- src/main/kotlin/org/meowcat/mesagisto/client/Logger.kt
+++ src/main/kotlin/org/meowcat/mesagisto/client/Logger.kt
@@ -12,38 +12,33 @@ object Logger {
   var provider: ILogger? = null
 
   inline fun trace(msg: () -> String) {
-    if (level >= LogLevel.TRACE) {
+    if (level <= LogLevel.TRACE) {
       provider?.log(LogLevel.TRACE, msg()) ?: println(msg())
     }
   }
 
   inline fun debug(msg: () -> String) {
-    if (level >= LogLevel.DEBUG) {
+    if (level <= LogLevel.DEBUG) {
       provider?.log(LogLevel.DEBUG, msg()) ?: println(msg())
     }
   }
 
   inline fun info(msg: () -> String) {
-    if (level >= LogLevel.INFO) {
+    if (level <= LogLevel.INFO) {
       provider?.log(LogLevel.INFO, msg()) ?: println(msg())
     }
   }
 
   inline fun warn(msg: () -> String) {
-    if (level >= LogLevel.WARN) {
+    if (level <= LogLevel.WARN) {
       provider?.log(LogLevel.WARN, msg()) ?: println(msg())
     }
   }
 
   inline fun error(msg: () -> String) {
-    if (level >= LogLevel.ERROR) {
+    if (level <= LogLevel.ERROR) {
       provider?.log(LogLevel.ERROR, msg()) ?: println(msg())
     }
   }
-  inline fun error(e: Throwable) {
-    if (level >= LogLevel.ERROR) {
-      provider?.log(LogLevel.ERROR, "Error ${e.message} \n ${e.stackTrace}")
-        ?: println("${e.message} \n ${e.stackTrace}")
-    }
-  }
+  inline fun error(e: Throwable) = error { "Error ${e.message} \n ${e.stackTraceToString()}" }
 }
