24dc412f77adf2ac3ac7832c7c0bafe0da8e4ce4
itsusinn <itsusinn@foxmail.com>
2021-11-14 15:40:26 +0800

    style: rename fields' case

--- src/main/kotlin/org/meowcat/mesagisto/client/Cipher.kt
+++ src/main/kotlin/org/meowcat/mesagisto/client/Cipher.kt
@@ -10,9 +10,9 @@ import kotlin.concurrent.getOrSet
 import kotlin.properties.Delegates
 
 object Cipher {
-  private lateinit var KEY: KeyParameter
-  internal lateinit var RAW_KEY: String
-  private val SECURE_RANDOM: SecureRandom by lazy { SecureRandom() }
+  private lateinit var key: KeyParameter
+  private lateinit var rawKey: String
+  private val secureRandom: SecureRandom by lazy { SecureRandom() }
   var ENABLE by Delegates.notNull<Boolean>()
     private set
   var REFUSE_PLAIN by Delegates.notNull<Boolean>()
@@ -20,23 +20,23 @@ object Cipher {
   private val inner: ThreadLocal<GCMBlockCipher> = ThreadLocal()
   fun newNonce(): ByteArray {
     val nonce = ByteArray(12)
-    SECURE_RANDOM.nextBytes(nonce)
+    secureRandom.nextBytes(nonce)
     return nonce
   }
   fun init(key: String, refusePlain: Boolean = true) {
     ENABLE = true
     REFUSE_PLAIN = refusePlain
-    RAW_KEY = key
+    rawKey = key
     val digest = MessageDigest.getInstance("SHA-256")
     val key256 = digest.digest(key.toByteArray())
-    KEY = KeyParameter(key256)
+    this.key = KeyParameter(key256)
   }
   fun deinit() {
     ENABLE = false
   }
   fun encrypt(plaintext: ByteArray, nonce: ByteArray): ByteArray {
     val cipher = inner.getOrSet { GCMBlockCipher(AESEngine()) }
-    val key = AEADParameters(KEY, 128, nonce)
+    val key = AEADParameters(key, 128, nonce)
     val encryptedBytes = with(cipher) {
       init(true, key)
       val res = ByteArray(getOutputSize(plaintext.size))
@@ -50,7 +50,7 @@ object Cipher {
 
   fun decrypt(ciphertext: ByteArray, nonce: ByteArray): ByteArray {
     val cipher = inner.getOrSet { GCMBlockCipher(AESEngine()) }
-    val key = AEADParameters(KEY, 128, nonce)
+    val key = AEADParameters(key, 128, nonce)
     val plainBytes = with(cipher) {
       cipher.init(false, key)
       val res = ByteArray(getOutputSize(ciphertext.size))
