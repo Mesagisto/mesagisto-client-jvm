71677f279e0a72007a806411293be4f796248e19
itsusinn <itsusinn@foxmail.com>
2021-12-14 17:41:56 +0800

    chore: release 1.0.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ publishing {
       from(components["java"])
       groupId = "org.meowcat"
       artifactId = "mesagisto-client"
-      version = "1.0.12"
+      version = "1.0.13"
     }
   }
 }
