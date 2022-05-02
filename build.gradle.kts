6851c22becef451c42b8c780b33d50f144f71832
itsusinn <itsusinn@foxmail.com>
2022-05-02 18:25:09 +0800

    chore: release 1.3.2 & update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.6.0"
-  kotlin("plugin.serialization") version "1.6.0"
+  id("org.jetbrains.kotlin.jvm") version "1.6.21"
+  kotlin("plugin.serialization") version "1.6.21"
   `java-library`
   `maven-publish` // Jitpack
 }
@@ -17,7 +17,7 @@ publishing {
       from(components["java"])
       groupId = "org.meowcat"
       artifactId = "mesagisto-client"
-      version = "1.3.1"
+      version = "1.3.2"
     }
   }
 }
@@ -39,7 +39,9 @@ dependencies {
   compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
   compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
   implementation("org.rocksdb:rocksdbjni:7.0.4")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.2.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.2")
   implementation("io.nats:jnats:2.14.0")
   implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+  implementation("com.charleskorn.kaml:kaml:0.43.0")
+  implementation("org.snakeyaml:snakeyaml-engine:2.3")
 }
