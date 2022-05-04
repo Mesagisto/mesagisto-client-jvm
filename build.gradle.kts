7c17b69c703f11e0ef0820d566e1af52f4e4ca41
itsusinn <itsusinn@foxmail.com>
2022-05-04 15:27:10 +0800

    build: downgrade kotlinx.serde

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.6.21"
-  kotlin("plugin.serialization") version "1.6.21"
+  id("org.jetbrains.kotlin.jvm") version "1.6.0"
+  kotlin("plugin.serialization") version "1.6.0"
   `java-library`
   `maven-publish` // Jitpack
 }
@@ -29,7 +29,6 @@ repositories {
 tasks.compileKotlin {
   kotlinOptions {
     jvmTarget = "1.8"
-    freeCompilerArgs = listOf("-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
   }
   sourceCompatibility = "1.8"
 }
@@ -38,8 +37,8 @@ dependencies {
 
   compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
   compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
-  implementation("org.rocksdb:rocksdbjni:7.0.4")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.2")
+  implementation("org.rocksdb:rocksdbjni:7.1.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.2.2")
   implementation("io.nats:jnats:2.14.0")
   implementation("org.bouncycastle:bcprov-jdk15on:1.70")
   implementation("com.charleskorn.kaml:kaml:0.43.0")
