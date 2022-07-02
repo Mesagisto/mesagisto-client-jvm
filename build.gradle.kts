a4fed26924384d3c54364026509c5df3207dae8b
Itsusinn <itsusinn@meowcat.org>
2022-07-02 13:36:31 +0800

    ci: nexus auto staging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("org.jetbrains.kotlin.jvm") version "1.6.0"
   id("me.him188.maven-central-publish") version "1.0.0-dev-3"
+  id("io.codearte.nexus-staging") version "0.30.0"
 }
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
@@ -10,6 +11,12 @@ group = "org.mesagisto"
 version = "1.4.3"
 
 mavenCentralPublish {
+  nexusStaging {
+    serverUrl = "https://s01.oss.sonatype.org/service/local/"
+    stagingProfileId = "9bdaa8e9e83392"
+    username = credentials?.sonatypeUsername
+    password = credentials?.sonatypePassword
+  }
   useCentralS01()
   githubProject("Mesagisto", "mesagisto-client-jvm")
   licenseFromGitHubProject("LGPL-2.1", "master")
